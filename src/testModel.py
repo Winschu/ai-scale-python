b941b146ced360ea407707e708abd61f4c705613
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2024-02-06 20:04:46 +0100

    Removed unused variables and fixed paths

--- src/testModel.py
+++ src/testModel.py
@@ -4,24 +4,18 @@ import numpy as np
 
 import tensorflow as tf
 
-use_label_file = False  # set this to true if you want load the label names from a file; uses the label_file defined below; the file should contain the names of the used labels, each label on a separate line
 label_file = 'labels.txt'
-base_dir = '../../../..'  # relative path to the Fruit-Images-Dataset folder
-train_dir = os.path.join(base_dir, 'Training')
-#test_dir = os.path.join(base_dir, 'Test')
-saved_files = os.getcwd() + '/output_files'  # root folder in which to save the the output files; the files will be under output_files/model_name
+train_dir = './data/Training'
+saved_files = './output_files'
 
 if not os.path.exists(saved_files):
     os.makedirs(saved_files)
 
     labels = os.listdir(train_dir)
 
-
-
-
 def test_model(path: str):
     # Load the model once, outside your function
-    name = 'fruit-360-model-gpu-30ep-100ba-reduced-labels'
+    name = 'model'
     model_out_dir = os.path.join(saved_files, name)
     if not os.path.exists(model_out_dir):
         print("No saved model found in: " + model_out_dir)
@@ -35,6 +29,3 @@ def test_model(path: str):
     print("Prediction probabilities: " + str(y_pred))
     print("Predicted class index: " + str(y_pred.argmax(axis=-1)))
     print("Predicted class label: " + labels[y_pred.argmax(axis=-1)[0]])
-
-
-test_model()
